fd93b4279c90dfade9dba99b315b775485e3539c
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-20 23:07:53 +0300

    + ComplexTypes.scala

---
+++ src/main/scala/part3typesdatasets/ComplexTypes.scala
@@ -0,0 +1,32 @@
+package part3typesdatasets
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+
+object ComplexTypes extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Complex Data Types")
+    .config("spark.master", "local")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  // Dates
+
+  val moviesWithReleaseDates = moviesDF
+    .select(col("Title"), to_date(col("Release_Date"), "dd-MMM-yy").as("Actual_Release")) // conversion
+  moviesWithReleaseDates.show()
+
+  moviesWithReleaseDates
+    .withColumn("Today", current_date()) // today
+    .withColumn("Right_Now", current_timestamp()) // this second
+    .withColumn("Movie_Age", datediff(col("Today"), col("Actual_Release")) / 365) // date_add, date_sub
+
+  moviesWithReleaseDates.select("*").where(col("Actual_Release").isNull)
+
+}
