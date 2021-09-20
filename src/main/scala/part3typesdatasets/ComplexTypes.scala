ac54e82561b5d3b607f4c7a47ac6fc8a1f3a1152
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-20 23:27:52 +0300

    + ComplexTypes.scala +ex

--- src/main/scala/part3typesdatasets/ComplexTypes.scala
+++ src/main/scala/part3typesdatasets/ComplexTypes.scala
@@ -9,6 +9,7 @@ object ComplexTypes extends App {
   val spark = SparkSession.builder()
     .appName("Complex Data Types")
     .config("spark.master", "local")
+    .config("spark.sql.legacy.timeParserPolicy","LEGACY")
     .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
 
@@ -28,5 +29,45 @@ object ComplexTypes extends App {
     .withColumn("Movie_Age", datediff(col("Today"), col("Actual_Release")) / 365) // date_add, date_sub
 
   moviesWithReleaseDates.select("*").where(col("Actual_Release").isNull)
+
+  /**
+    * Exercise
+    * 1. How do we deal with multiple date formats?
+    * 2. Read the stocks DF and parse the dates
+    */
+
+  // 1 - parse the DF multiple times, then union the small DFs
+
+  // 2
+  val stocksDF = spark.read.format("csv")
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .load("src/main/resources/data/stocks.csv")
+
+  val stocksDFWithDates = stocksDF
+    .withColumn("actual_date", to_date(col("date"), "MMM dd yyyy"))
+
+  // Structures
+
+  // 1 - with col operators
+  moviesDF
+    .select(col("Title"), struct(col("US_Gross"), col("Worldwide_Gross")).as("Profit"))
+    .select(col("Title"), col("Profit").getField("US_Gross").as("US_Profit"))
+
+  // 2 - with expression strings
+  moviesDF
+    .selectExpr("Title", "(US_Gross, Worldwide_Gross) as Profit")
+    .selectExpr("Title", "Profit.US_Gross")
+
+  // Arrays
+
+  val moviesWithWords = moviesDF.select(col("Title"), split(col("Title"), " |,").as("Title_Words")) // ARRAY of strings
+
+  moviesWithWords.select(
+    col("Title"),
+    expr("Title_Words[0]"), // indexing
+    size(col("Title_Words")), // array size
+    array_contains(col("Title_Words"), "Love") // look for value in array
+  )
 
 }
