a7f155bc409550b3d46a2da07a8c83f51fec5250
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-22 10:55:11 +0300

    + RDDs.scala ex

--- src/main/scala/part5lowlevel/RDDs.scala
+++ src/main/scala/part5lowlevel/RDDs.scala
@@ -92,4 +92,79 @@ object RDDs extends App {
     .mode(SaveMode.Overwrite)
     .parquet("src/main/resources/data/stocks15")
 
+  /**
+    * Exercises
+    *
+    * 1. Read the movies.json as an RDD.
+    * 2. Show the distinct genres as an RDD.
+    * 3. Select all the movies in the Drama genre with IMDB rating > 6.
+    * 4. Show the average rating of movies by genre.
+    */
+
+  case class Movie(title: String, genre: String, rating: Double)
+
+  // 1
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  val moviesRDD = moviesDF
+    .select(col("Title").as("title"), col("Major_Genre").as("genre"), col("IMDB_Rating").as("rating"))
+    .where(col("genre").isNotNull and col("rating").isNotNull)
+    .as[Movie]
+    .rdd
+
+  // 2
+  val genresRDD = moviesRDD.map(_.genre).distinct()
+
+  // 3
+  val goodDramasRDD = moviesRDD.filter(movie => movie.genre == "Drama" && movie.rating > 6)
+
+  // 4
+  case class GenreAvgRating(genre: String, rating: Double)
+
+  val avgRatingByGenreRDD = moviesRDD.groupBy(_.genre).map {
+    case (genre, movies) => GenreAvgRating(genre, movies.map(_.rating).sum / movies.size)
+  }
+
+  avgRatingByGenreRDD.toDF.show
+  moviesRDD.toDF.groupBy(col("genre")).avg("rating").show
 }
+
+/*
+  reference:
+    +-------------------+------------------+
+    |              genre|       avg(rating)|
+    +-------------------+------------------+
+    |          Adventure| 6.345019920318729|
+    |              Drama| 6.773441734417339|
+    |        Documentary| 6.997297297297298|
+    |       Black Comedy|6.8187500000000005|
+    |  Thriller/Suspense| 6.360944206008582|
+    |            Musical|             6.448|
+    |    Romantic Comedy| 5.873076923076922|
+    |Concert/Performance|             6.325|
+    |             Horror|5.6760765550239185|
+    |            Western| 6.842857142857142|
+    |             Comedy| 5.853858267716529|
+    |             Action| 6.114795918367349|
+    +-------------------+------------------+
+
+  RDD:
+    +-------------------+------------------+
+    |              genre|            rating|
+    +-------------------+------------------+
+    |Concert/Performance|             6.325|
+    |            Western| 6.842857142857142|
+    |            Musical|             6.448|
+    |             Horror|5.6760765550239185|
+    |    Romantic Comedy| 5.873076923076922|
+    |             Comedy| 5.853858267716529|
+    |       Black Comedy|6.8187500000000005|
+    |        Documentary| 6.997297297297298|
+    |          Adventure| 6.345019920318729|
+    |              Drama| 6.773441734417339|
+    |  Thriller/Suspense| 6.360944206008582|
+    |             Action| 6.114795918367349|
+    +-------------------+------------------+
+ */
